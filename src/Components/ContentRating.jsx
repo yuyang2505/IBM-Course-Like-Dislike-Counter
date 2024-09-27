1865689e718616797d2d28f5693bf74479a3ea7e
yuyang <ongyuyang2505@gmail.com>
2024-09-27 10:12:50 -0400

    Initial Commit

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,16 +1,43 @@
-
 import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes: 0,
+      dislikes: 0
+    };
   }
+
+  handleLike = () => {
+    this.setState((prevState) => ({
+      likes: prevState.likes + 1
+    }));
+  };
+
+  handleDislike = () => {
+    this.setState((prevState) => ({
+      dislikes: prevState.dislikes + 1
+    }));
+  };
+
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      <>
+        <h1>Text Content Rating</h1>
+        <div className='content-rating'>
+          <p>Text</p>
+          <div className='rating-buttons'>
+            <button className="like-button" onClick={this.handleLike}>
+              Like ({this.state.likes})
+            </button>
+            <button className="dislike-button" onClick={this.handleDislike}>
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
+        </div>
+      </>
     );
   }
 }
